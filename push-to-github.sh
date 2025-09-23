dccfa1e6b2d9a72cfed67afe186299c23659e4f3
arcolinuxz <arcolinuxinfo@gmail.com>
2025-09-23 19:37:47 -0400

    Add all new files

---
+++ push-to-github.sh
@@ -0,0 +1,4 @@
+cd /home/abdou/abdou-chadwm/
+git add .                                                                                                                                             Tue 23 Sep 2025 07:28:45 PM EDT
+git commit -m "Add all new files"
+git push
